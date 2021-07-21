d9993ed1c34f348b48d258b6bb7a303d5b040681
Alexander Zadykian <zadykian@outlook.com>
2021-07-21 22:26:40 +0300

    IVerySeriousBusines

---
+++ src/Bars.Homework.MemoryManagement/Controllers/SomeBizController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bars.Homework.MemoryManagement.Controllers
+{
+	/// <summary>
+	/// Biz controller.
+	/// </summary>
+	public class SomeBizController : ControllerBase
+	{
+		/// <summary>
+		/// Do some very serious business asynchronously.
+		/// </summary>
+		public async Task<IActionResult> DoSomeSeriousBusiness()
+		{
+			await Task.CompletedTask;
+			return Ok();
+		}
+	}
+}
---
+++ src/Bars.Homework.MemoryManagement/Services/IVerySeriousBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Bars.Homework.MemoryManagement.Services
+{
+	/// <summary>
+	/// A very serious business service.
+	/// </summary>
+	internal interface IVerySeriousBusiness
+	{
+		/// <summary>
+		/// Do it, yesterday you said tomorrow.
+		/// </summary>
+		Task DoItAsync();
+	}
+}
